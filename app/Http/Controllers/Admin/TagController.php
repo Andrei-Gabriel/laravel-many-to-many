5bf39cede36119520ba6abf10316bad8617ff9c9
Andrei-Gabriel <andrescu.gabriel.andrei@gmail.com>
2022-02-22 00:38:11 +0100

    Delete di Tag

--- app/Http/Controllers/Admin/TagController.php
+++ app/Http/Controllers/Admin/TagController.php
@@ -108,6 +108,7 @@ class TagController extends Controller
      */
     public function destroy(Tag $tag)
     {
-        //
+        $tag->delete();
+        return redirect()->route("tags.index");
     }
 }
